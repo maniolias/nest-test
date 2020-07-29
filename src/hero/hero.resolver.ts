0350ac4f2bb4776bba947fd32b4d3d0565caed8c
cedricATBQ <cedric.rumiano@atbq.fr>
2020-07-29 16:16:10 +0200

    fix: add queries for root types Human and Droid

--- src/hero/hero.resolver.ts
+++ src/hero/hero.resolver.ts
@@ -1,5 +1,7 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Character, Episode } from './types/character.type';
+import { Human } from './types/human.type';
+import { Droid } from './types/droid.type';
 
 @Resolver('Hero')
 export class HeroResolver {
@@ -10,5 +12,21 @@ export class HeroResolver {
   )
   async hero(@Args('episode', { type: () => Episode }) episode: Episode): Promise<Character[]> {
     return this.heroes.filter(hero => hero.appearsIn.indexOf(episode) !== -1);
+  }
+
+  @Query(
+    /* istanbul ignore next */
+    (): [typeof Human] => [Human],
+  )
+  async humans(): Promise<Human[]> {
+    return this.heroes.filter(hero => hero instanceof Human) as Human[];
+  }
+
+  @Query(
+    /* istanbul ignore next */
+    (): [typeof Droid] => [Droid],
+  )
+  async droids(): Promise<Droid[]> {
+    return this.heroes.filter(hero => hero instanceof Droid) as Droid[];
   }
 }
