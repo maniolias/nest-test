98fe6b288894b88feda549d1f75ddd6acc2c55a8
cedricATBQ <cedric.rumiano@atbq.fr>
2020-07-29 16:16:54 +0200

    feat(hero): add resolveType function to interface

--- src/hero/types/character.type.ts
+++ src/hero/types/character.type.ts
@@ -1,6 +1,8 @@
 import { Field, InterfaceType, registerEnumType } from '@nestjs/graphql';
 
 import { CustomID } from '../scalars/customID.scalar';
+import { Human } from './human.type';
+import { Droid } from './droid.type';
 
 export enum Episode {
   NEWHOPE,
@@ -12,7 +14,15 @@ registerEnumType(Episode, {
   name: 'Episode',
 });
 
-@InterfaceType()
+@InterfaceType({
+  resolveType: (character)=> {
+    if (character.totalCredits) {
+      return Human;
+    } else {
+      return Droid;
+    }
+  },
+})
 export abstract class Character {
   @Field(() => CustomID)
   id: CustomID;
